61dfb7b894b602261a9692cbc9d354fa03dbbc5a
Langster2323 <langahkeem@gmail.com>
2020-04-17 08:37:54 -0500

    Creating product and order routes

---
+++ api/routes/products.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Handling GET requirests to /products'
+    })
+})
+router.post('/', (req, res, next) => {
+    res.status(201).json({
+        message: 'Handling POST requirests to /products'
+    })
+})
+
+router.get('/:productId', (req, res, next) => {
+    const id = req.params.productId;
+    if (id === 'special') {
+        res.status(200).json({
+            message: 'You discoved the special ID',
+            id: id
+        })
+    } else {
+        res.status(200).json({
+            message: 'You passed an ID'
+        })
+    }
+})
+router.patch('/:productId', (req, res, next) => {
+    res.status(200).json({
+        message: "Updated product"
+    })
+})
+router.delete('/:productId', (req, res, next) => {
+    res.status(200).json({
+        message: "Deleted product"
+    })
+})
+
+
+module.exports = router;
