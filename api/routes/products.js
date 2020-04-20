0907647f4c10bd7ebbbd700a9dc78a9bf1232f35
Langster2323 <langahkeem@gmail.com>
2020-04-20 08:41:42 -0500

    Fixed typo also connected mongoose to application.

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const Product = require('../models/product');
 
 router.get('/', (req, res, next) => {
     res.status(200).json({
@@ -7,15 +9,19 @@ router.get('/', (req, res, next) => {
     })
 })
 router.post('/', (req, res, next) => {
-    const product = {
+    const product = new Product({
+        _id: new mongoose.Types.ObjectId(),
         name: req.body.name,
         price: req.body.price
-    }
+    });
+    product.save().then(result => {
+        console.log(result);
+    }).catch(err => console.log(err));
     res.status(201).json({
         message: 'Handling POST requirests to /products',
         createdProduct: product
-    })
-})
+    });
+});
 
 router.get('/:productId', (req, res, next) => {
     const id = req.params.productId;
--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products')
 const orderRouters = require('./api/routes/orders');
@@ -10,7 +11,7 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-app.use((req, res, mext) => {
+app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     if (req.method === 'OPTIONS') {
@@ -23,6 +24,16 @@ app.use((req, res, mext) => {
 // Routes which should handle requests
 app.use('/products', productRoutes)
 app.use('/orders', orderRouters)
+
+mongoose.connect(
+    'mongodb+srv://Langster2323:' + 
+    process.env.MONGO_ATLAS_PW + 
+    '@cluster0-x9jrv.mongodb.net/test?retryWrites=true&w=majority', 
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+);
 
 app.use((req, res, next) => {
     const error = new Error('Not Found');
