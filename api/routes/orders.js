5409d9f89c850a4b814547d0bc5c610a4cb89960
Langster2323 <langahkeem@gmail.com>
2020-04-21 10:42:21 -0500

    Created order model. updated post and get methods for orders

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,20 +1,45 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const Order = require('../models/order');
 
 // Handle incoming GET requests to /orders
 router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Orders were fetched'
+    Order.find()
+    .select('quantity product _id')
+    .exec()
+    .then(docs => {
+        res.status(200).json(docs)
+    })
+    .catch(err => {
+        res.status(500).json({error: err})
     })
 })
 router.post('/', (req, res, next) => {
-    const order = {
-        productId: req.body.productId,
+    const order = new Order({
+        _id: new mongoose.Types.ObjectId(),
+        product: req.body.productId,
         quantity: req.body.quantity
-    }
-    res.status(201).json({
-        message: 'Orders were created',
-        createdOrder: order
+    });
+    order.save()
+    .then(result => {
+        console.log(result);
+        
+        res.status(201).json({
+            message: 'Orders were created',
+            createdOrder: {
+                product: result.productId,
+                quantity: result.quantity,
+                _id: result._id,
+                request: {
+                    type: 'GET',
+                    url: 'http://localhost:3000/orders' + result._id
+                }
+            }
+        })
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json({error: err})
     })
 })
 router.get('/:orderId', (req, res, next) => {
