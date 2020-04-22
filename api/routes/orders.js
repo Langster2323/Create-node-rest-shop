fe2d47de3ab78c086bfc5ec28540dc77323d3b08
Langster2323 <langahkeem@gmail.com>
2020-04-22 07:51:17 -0500

    using jwt concept for authentication flow. routes are protected

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -3,9 +3,10 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const Order = require('../models/order');
 const Product = require('../models/product');
+const checkAuth = require('../auth/check-auth');
 
 // Handle incoming GET requests to /orders
-router.get('/', (req, res, next) => {
+router.get('/', checkAuth, (req, res, next) => {
     Order.find()
     .select('quantity product _id')
     .populate('product')
@@ -30,7 +31,7 @@ router.get('/', (req, res, next) => {
         res.status(500).json({error: err})
     })
 })
-router.post('/', (req, res, next) => {
+router.post('/', checkAuth, (req, res, next) => {
     Product.findById(req.body.productId)
     .then(product => {
         if (!product) {
@@ -64,7 +65,7 @@ router.post('/', (req, res, next) => {
         res.status(500).json({error: err})
     })
 })
-router.get('/:orderId', (req, res, next) => {
+router.get('/:orderId', checkAuth, (req, res, next) => {
     Order.findById(req.params.orderId)
     .populate('product')
     .exec()
@@ -86,7 +87,7 @@ router.get('/:orderId', (req, res, next) => {
         res.status(500).json({error: err})
     })
 })
-router.delete('/:orderId', (req, res, next) => {
+router.delete('/:orderId', checkAuth, (req, res, next) => {
     Order.remove({ _id: req.params.orderId })
     .exec()
     .then(result => {
--- api/routes/products.js
+++ api/routes/products.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const Product = require('../models/product');
 const multer = require('multer');
+const checkAuth = require('../auth/check-auth');
+
 const storage = multer.diskStorage({
     destination: function(req, file, cb) {
         cb(null, './uploads/');
@@ -58,7 +60,7 @@ router.get('/', (req, res, next) => {
         res.status(500).json({error: err})
     })
 })
-router.post('/', upload.single('productImage'), (req, res, next) => {
+router.post('/', checkAuth, upload.single('productImage'), (req, res, next) => {
     const product = new Product({
         _id: new mongoose.Types.ObjectId(),
         name: req.body.name,
@@ -111,8 +113,9 @@ router.get('/:productId', (req, res, next) => {
         console.log(err);
         res.status(500).json({error: err})
     });
-})
-router.patch('/:productId', (req, res, next) => {
+});
+
+router.patch('/:productId', checkAuth, (req, res, next) => {
     const id = req.params.productId
     const updateOps = {};
     for (const ops of req.body) {
@@ -136,7 +139,7 @@ router.patch('/:productId', (req, res, next) => {
         res.status(500).json({error: err})
     })
 })
-router.delete('/:productId', (req, res, next) => {
+router.delete('/:productId', checkAuth, (req, res, next) => {
     const id = req.params.productId
     Product.remove({_id: id})
     .exec()
